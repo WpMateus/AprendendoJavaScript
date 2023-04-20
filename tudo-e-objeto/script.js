2426bd57887f3700121afc0015f62db136c59863
Mateus <mateuswenzler@hotmail.com>
2023-04-19 21:52:00 -0300

    Estudando mais JavaScript

---
+++ tudo-e-objeto/script.js
@@ -0,0 +1,28 @@
+// var nome = 'MatEus';
+
+// var nomeMinusculo = nome.toLowerCase();
+
+// var btn = document.querySelector('.btn');
+
+//======================================================
+
+// nomeie 3 propriedades ou métodos de strings
+var nome = 'Mateus';
+// fixed
+// length
+// slice
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+var btn = document.querySelector('.btn');
+// addEventListener
+//id
+//innerHtml
+//appendChild
+//outerHtml
+
+
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
+
---
+++ variaveis/script.js
@@ -0,0 +1,13 @@
+var nome = 'Mateus';
+var idade = 20;
+var comidaFavorita;
+
+comidaFavorita = "Churrasco";
+
+var cidade,
+    conscienciaClasse,
+    faculdade,
+    dinheiro,
+    filmeFavorito;
+
+console.log(nome, idade, comidaFavorita, cidade, conscienciaClasse, faculdade, dinheiro, filmeFavorito);
