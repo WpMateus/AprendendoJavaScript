c8e73c9bd205ee0e3ae4ce76200897afe3d38e40
Mateus <mateuswenzler@hotmail.com>
2023-04-30 11:42:08 -0300

    estudos javascript

---
+++ Dom-Para-Iniciantes/Classes e Atributos - Animais Fantasticos - Copia - Copia/script.js
@@ -0,0 +1,24 @@
+// const menu = document.querySelector('.menu');
+
+// menu.classList.add('ativo', 'teste');
+// menu.classList.remove('azul');
+// menu.classList.toggle('azul');
+
+// if(menu.classList.contains('azul')){
+//   menu.classList.add('possui-azul');
+// }else{
+//   menu.classList.add('nao-possui-azul');
+// }
+
+// // console.log(menu.classList);
+
+// const animais = document.querySelector('.animais');
+// console.log(animais.attributes[1]);
+
+// const img = document.querySelector('img');
+
+// const srcImg = img.getAttribute('alt')
+
+// img.setAttribute('alt', 'É uma raposa')
+
+// console.log(srcImg);
--- Dom-Para-Iniciantes/forEach e Arrow Function - Animais Fantasticos - Copia/script.js
+++ Dom-Para-Iniciantes/forEach e Arrow Function - Animais Fantasticos - Copia/script.js
@@ -1,24 +1,38 @@
+// // const imgs = document.querySelectorAll('img');
+
+// // imgs.forEach(function(item, index, array){
+// //   // console.log(item, index, array);
+// // });
+
+// const titulos = document.getElementsByClassName('titulo');
+// const titulosArray = Array.from(titulos);
+
+// titulosArray.forEach(function(item){
+//   // console.log(item);
+// });
+
+
 // const imgs = document.querySelectorAll('img');
 
-// imgs.forEach(function(item, index, array){
-//   // console.log(item, index, array);
-// });
+// // imgs.forEach((item) =>{
+// //   console.log(item);
+// // });
 
-const titulos = document.getElementsByClassName('titulo');
-const titulosArray = Array.from(titulos);
-
-titulosArray.forEach(function(item){
-  // console.log(item);
-});
-
-
-const imgs = document.querySelectorAll('img');
-
+// let i = 0;
 // imgs.forEach((item) =>{
-//   console.log(item);
+//     console.log(i++);
 // });
 
-let i = 0;
-imgs.forEach((item) =>{
-    console.log(i++);
-});
+
+// //Caso seja assim é nececessário tirar {} e sem ponto e virgula no final, quando for apenas uma linha
+// imgs.forEach((item) =>
+//   console.log(i++)
+// );
+
+
+// //constantemente vamos ver um código parecido com isso aqui
+// imgs.forEach(function(){ 
+//   console.log(i++) 
+// }); //codigo era assim
+
+// imgs.forEach(() => console.log(i++)); // ficou assim
