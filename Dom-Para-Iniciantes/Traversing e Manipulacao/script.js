792b4b389c193689d3b02bc644459c7cc61943e2
Mateus Wenzler <mateuswenzler@hotmail.com>
2023-06-12 17:54:21 -0300

    Eventos e Manipulação

---
+++ Dom-Para-Iniciantes/Traversing e Manipulacao/script.js
@@ -0,0 +1,34 @@
+// const h1 = document.querySelector('h1');
+// const animaisLista = document.querySelector('.animais-descricao');
+
+// h1.innerHTML = '<p>Novo Título</p>';
+
+// console.log(animaisLista.innerHTML);
+
+// const lista = document.querySelector('.animais-lista');
+
+// console.log(lista.children[lista.children.length - 1]);
+
+// const animais = document.querySelector('.animais')
+// const contato = document.querySelector('.contato');
+// const titulo = contato.querySelector('.titulo');
+// const mapa = contato.querySelector('.mapa');
+
+// contato.insertBefore(animais, mapa);
+
+// const novoh1 = document.createElement('h1');
+
+// novoh1.innerText = 'Novo Título';
+// novoh1.classList.add('titulo');
+// mapa.appendChild(novoh1);
+
+// const h1 = document.querySelector('h1');
+
+// const faq = document.querySelector('.faq');
+
+// const cloneH1 = h1.cloneNode(true);
+
+// cloneH1.classList.add('azul');
+
+// faq.appendChild(cloneH1);
+//faq.appendChild(h1); // Movi o h1 e não clonei
